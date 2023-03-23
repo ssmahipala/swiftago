96432a98db999f8f8f0ccd4016878ab36838b14e
ssmahipala <saisumanth.mahipala@outlook.com>
2023-03-23 02:16:49 -0500

    server-and-userRoutes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log("Server Initializing...")
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 8000
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.status(200).send('Swiftago - Crafting user stories for you...Effortlessly')
+})
+
+//Routes
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.listen(PORT, () => console.log(`Server Initialized on PORT ${PORT}`))
